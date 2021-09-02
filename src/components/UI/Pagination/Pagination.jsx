a0d695612f09f6da927d05644cbf48aab390bd8e
Вадим Бабичев <babichevvadim@MacBook-Air-Vadim.local>
2021-09-02 22:14:01 +0300

    Замена -- и ++ на - 1 и + 1 в компоненте Pagination

--- src/components/UI/Pagination/Pagination.jsx
+++ src/components/UI/Pagination/Pagination.jsx
@@ -18,13 +18,13 @@ const Pagination = ({
   const onChangePage = (changeFunc, page, direction) => {
     event.preventDefault();
     if (page > 1 && direction === 'back' && !beerItemsLoading)
-      changeFunc(--page);
+      changeFunc(page - 1);
     if (
       direction === 'next' &&
       beers.length === beersOnPage &&
       !beerItemsLoading
     )
-      changeFunc(++page);
+      changeFunc(page + 1);
   };
 
   useEffect(() => {
