276f5296bcfbc78c16f23709d6499b7ac392dd37
Вадим Бабичев <babichevvadim@MacBook-Air-Vadim.local>
2021-09-04 22:21:32 +0300

    Исправление верстки Button

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -5,9 +5,9 @@ const Button = ({ btnText, func, arg }) => {
   let classNames = 'btn';
 
   return (
-    <a href='#' className={classNames} onClick={(e) => func(e, arg)}>
+    <button className={classNames} onClick={(e) => func(e, arg)}>
       {btnText}
-    </a>
+    </button>
   );
 };
 
